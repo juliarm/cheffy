c5bdb3f325baf2b44391d6d5e76e769b529d54bc
Julia Roytman <juliaroytmin@Julias-MacBook-Air.local>
2014-11-19 16:18:49 -0500

    Initial Commit

---
+++ myScript.js
@@ -0,0 +1,4 @@
+alert("Welcome");
+
+//Do I need to make different js files for each of the restaurant pages? check url, keep one doc
+//window.location.href if it contains index.html do x, otherwise do y
